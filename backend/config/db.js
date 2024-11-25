5ba6c2fa412dce37bedc8aee017d0f4c6e56eb46
neeldholiya04 <neeldholiya04@gmail.com>
2024-11-25 21:34:24 +0530

    project completed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,25 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
-  logging: false,
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
